7847b9fa43c466c46be210ec3457d086dd87c522
Fredwalcott <fredwalcott18@gmail.com>
2026-06-30 03:04:57 +0000

    Shift notification modal to bottom-center

--- src/components/ui/site-notification-banner.tsx
+++ src/components/ui/site-notification-banner.tsx
@@ -75,7 +75,7 @@ export default function SiteNotificationBanner() {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.25 }}
-          className="fixed inset-0 z-[300] flex items-center justify-center p-4"
+          className="fixed inset-0 z-[300] flex items-center justify-center p-4 pt-[20vh]"
         >
           <motion.div
             initial={{ opacity: 0 }}
